89b9eef2705997ff948b49aed9a4b14a0a417ae2
Theo Goduout <theo.goudout@gmail.com>
2016-06-06 13:27:40 +0100

    Bug fixes and adding comments to cmake files

--- cmake_modules/FindCPPUNIT.cmake
+++ cmake_modules/FindCPPUNIT.cmake
@@ -2,17 +2,16 @@
 # Find the CppUnit includes and library
 #
 # This module defines
-# CPPUNIT_INCLUDE_DIR, where to find tiff.h, etc.
-# CPPUNIT_LIBRARIES, the libraries to link against to use CppUnit.
-# CPPUNIT_FOUND, If false, do not try to use CppUnit.
+# CPPUNIT_FOUND       , If false, do not try to use CppUnit.
+# CPPUNIT_INCLUDE_DIRS, where to find CppUnit header files
+# CPPUNIT_LIBRARIES   , the libraries to link against to use CppUnit.
 
-# also defined, but not for general use are
-# CPPUNIT_LIBRARY, where to find the CppUnit library.
-# CPPUNIT_DEBUG_LIBRARY, where to find the CppUnit library in debug
-# mode.
+# Find include path
+FIND_PATH(CPPUNIT_INCLUDE_DIR cppunit/TestCase.h
+    PATHS /usr/local/include /usr/include
+)
 
-FIND_PATH(CPPUNIT_INCLUDE_DIR cppunit/TestCase.h /usr/local/include /usr/include)
-
+# List of possible library paths
 SET(CPPUNIT_LIBRARY_PATHS
     ${CPPUNIT_INCLUDE_DIR}/../lib
     /usr/local/lib
@@ -21,30 +20,29 @@ SET(CPPUNIT_LIBRARY_PATHS
 
 IF(WIN32)
     # With Win32, important to have both
-    FIND_LIBRARY(CPPUNIT_LIBRARY cppunit
-        PATHS ${CPPUNIT_LIBRARY_PATHS}
-        PATH_SUFFIXES ${CMAKE_LIBRARY_ARCHITECTURE}
-    )
-
-    FIND_LIBRARY(CPPUNIT_DEBUG_LIBRARY cppunitd
-        PATHS ${CPPUNIT_LIBRARY_PATHS}
-        PATH_SUFFIXES ${CMAKE_LIBRARY_ARCHITECTURE}
-    )
-
+    IF(CMAKE_BUILD_TYPE == "RELEASE" || CMAKE_BUILD_TYPE == "MINSIZEREL")
+        FIND_LIBRARY(CPPUNIT_LIBRARY cppunit
+            PATHS ${CPPUNIT_LIBRARY_PATHS}
+            PATH_SUFFIXES ${CMAKE_LIBRARY_ARCHITECTURE}
+        )
+    ELSE()
+        FIND_LIBRARY(CPPUNIT_LIBRARY cppunitd
+            PATHS ${CPPUNIT_LIBRARY_PATHS}
+            PATH_SUFFIXES ${CMAKE_LIBRARY_ARCHITECTURE}
+        )
+    ENDIF()
 ELSE(WIN32)
     # On unix system, debug and release have the same name
     FIND_LIBRARY(CPPUNIT_LIBRARY cppunit
-        PATHS ${CPPUNIT_LIBRARY_PATHS}
+        PATHS ${CPPUNIT_INCLUDE_DIR}/../lib /usr/local/lib /usr/lib
         PATH_SUFFIXES ${CMAKE_LIBRARY_ARCHITECTURE}
     )
-
-    FIND_LIBRARY(CPPUNIT_DEBUG_LIBRARY cppunit
-        PATHS ${CPPUNIT_LIBRARY_PATHS}
-        PATH_SUFFIXES ${CMAKE_LIBRARY_ARCHITECTURE}
-    )
-
 ENDIF(WIN32)
 
+# Hide temporary variables
+MARK_AS_ADVANCED(CPPUNIT_INCLUDE_DIR CPPUNIT_LIBRARY)
+
+# Set output variables
 IF(NOT CPPUNIT_INCLUDE_DIR)
     SET(CPPUNIT_FOUND "NO")
     IF (CPPUNIT_FIND_REQUIRED)
@@ -57,6 +55,6 @@ ELSEIF(NOT CPPUNIT_LIBRARY)
     ENDIF(CPPUNIT_FIND_REQUIRED)
 ELSE()
     SET(CPPUNIT_FOUND "YES")
+    SET(CPPUNIT_INCLUDE_DIRS ${CPPUNIT_INCLUDE_DIR})
     SET(CPPUNIT_LIBRARIES ${CPPUNIT_LIBRARY} ${CMAKE_DL_LIBS})
-    SET(CPPUNIT_DEBUG_LIBRARIES ${CPPUNIT_DEBUG_LIBRARY} ${CMAKE_DL_LIBS})
 ENDIF()
