f827c1beb8ca37e8f7f599c8a6dc93dfc7117eb0
Théo Goudout <theo.goudout+github@gmail.com>
2017-02-25 17:58:37 +0100

    Add virtual method in writeable

--- include/writeable.h
+++ include/writeable.h
@@ -1,10 +1,29 @@
 #ifndef WRITEABLE_H_INCLUDED
 #define WRITEABLE_H_INCLUDED
 
+#include <ostream>
+
 namespace xml {
     template <typename charT>
     class basic_writeable {
+    public:
+                //! \name Member types
+        //!@{
+        typedef std::basic_ostream<charT> ostream_t;  //!< The type of stream to write into
 
+        typedef basic_writeable<charT> writeable_t; //!< The type of writeable.
+
+        //!@}
+
+        //! \brief Stream operator.
+        /*!
+         *  Write current writeable to the given output stream.
+         *
+         *  \param [in] os The output stream to write into.
+         *
+         *  \return The output stream given as parameter.
+         */
+        virtual ostream_t& operator<< (const ostream_t& os) = 0;
     };
 }
 
